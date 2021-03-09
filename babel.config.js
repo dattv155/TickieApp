9fbe4e3e52b5d0b4391c7cbd6a5f5a7c51eda1e8
TrongDatVu <trongdat1505@gmail.com>
2021-03-09 16:01:43 +0700

    Change some config

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'macros',
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        alias: {
-          assets: './assets/',
-          src: './src',
-          'app.json': './app.json',
-          'package.json': './package.json',
-        },
-      },
-    ],
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-  ],
+  // plugins: [
+  //   'macros',
+  //   [
+  //     'module-resolver',
+  //     {
+  //       root: ['.'],
+  //       alias: {
+  //         assets: './assets/',
+  //         src: './src',
+  //         'app.json': './app.json',
+  //         'package.json': './package.json',
+  //       },
+  //     },
+  //   ],
+  //   ['@babel/plugin-proposal-decorators', {legacy: true}],
+  // ],
 };

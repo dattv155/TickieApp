68460be5147c544a3cb097e46b3e0cf0066c170e
tannt <ngotientan811@gmail.com>
2021-03-19 00:33:22 +0700

    fix home screen ui

--- src/components/HomeComponent/UpcomingFilm/UpcomingFilm.tsx
+++ src/components/HomeComponent/UpcomingFilm/UpcomingFilm.tsx
@@ -1,10 +1,10 @@
-import React, { FC, PropsWithChildren, ReactElement } from 'react';
+import React, {FC, PropsWithChildren, ReactElement} from 'react';
 import nameof from 'ts-nameof.macro';
 import styles from './UpcomingFilm.scss';
 import {useState} from 'react';
 import {View, Text, FlatList, Image, Dimensions} from 'react-native';
 import {atomicStyles} from '../../../styles';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 /**
  * File: UpcomingFilm.tsx
  * @created 2021-03-16 20:30:56
@@ -17,62 +17,92 @@ const SLIDER_HEIGHT = Dimensions.get('window').height;
 const UpcomingFilm: FC<PropsWithChildren<UpcomingFilmProps>> = (
   props: PropsWithChildren<UpcomingFilmProps>,
 ): ReactElement => {
-  const [list, setList]= useState([
+  const [list, setList] = useState([
     {
       id: 1,
-      img: 'https://ae01.alicdn.com/kf/HTB1Va5mQXXXXXcnXXXXq6xXFXXXV/La-La-Land-Film-Aquarelle-Tissu-jet-d-encre-affiche-20-X13-07.jpg',
+      img:
+        'https://ae01.alicdn.com/kf/HTB1Va5mQXXXXXcnXXXXq6xXFXXXV/La-La-Land-Film-Aquarelle-Tissu-jet-d-encre-affiche-20-X13-07.jpg',
       name: 'La La Land',
-      release: '12-12-2021'
+      release: '12-12-2021',
     },
     {
       id: 2,
-      img: 'https://resizing.flixster.com/JSQhj07oIhsYdTaPu6iZ_ldKJa8=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2RlNzI0MGQyLTQ2ZTktNGMyYi05N2VmLTFjMDhiY2VlMDQ2Ni53ZWJw',
+      img:
+        'https://resizing.flixster.com/JSQhj07oIhsYdTaPu6iZ_ldKJa8=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2RlNzI0MGQyLTQ2ZTktNGMyYi05N2VmLTFjMDhiY2VlMDQ2Ni53ZWJw',
       name: 'Blade Runner 2049',
-      release: '12-2-2021'
+      release: '12-2-2021',
     },
     {
       id: 3,
-      img: 'https://fcine.net/uploads/monthly_2019_06/2pikachu-_vietnamese_poster.jpg.015075262656d06602221295e8ef16cf.jpg',
+      img:
+        'https://fcine.net/uploads/monthly_2019_06/2pikachu-_vietnamese_poster.jpg.015075262656d06602221295e8ef16cf.jpg',
       name: 'Detective Pikachu',
-      release: '12-02-2022'
+      release: '12-02-2022',
     },
     {
       id: 4,
-      img: 'https://i.pinimg.com/originals/a6/6d/93/a66d93b32698ef7d7f6aea369ab4d196.jpg',
+      img:
+        'https://i.pinimg.com/originals/a6/6d/93/a66d93b32698ef7d7f6aea369ab4d196.jpg',
       name: 'Demon Slayer: Infinitive train',
-      release: '12-02-2020'
-    }
+      release: '12-02-2020',
+    },
   ]);
-  const [translate]= useTranslation();
+  const [translate] = useTranslation();
   return (
-    <View style={styles.mainComponent}> 
-        <View>
-          <Text style={[atomicStyles.regular,styles.header]}>
-            {translate("homeScreen.upcoming")}
-          </Text>
-        </View>
-        <FlatList
-          data={list}
-          renderItem={({ item }) => (
-            <View style={{ alignItems: 'center', marginRight: SLIDER_WIDTH* 0.051}}> 
-                <Image
-                    style={{width: SLIDER_WIDTH *0.4,
-                            height: SLIDER_HEIGHT*0.32,
-                            borderRadius: 22
-                            }}
-                    source={{
-                      uri: item.img
-                    }}>
-                </Image>
-                <Text style={[atomicStyles.regular,styles.text1]}>{item.name}</Text>
-                <Text style={[atomicStyles.regular,styles.text2]}>{item.release}</Text>
-            </View>
-          )}
-          keyExtractor={item => item.id}
-          horizontal={true}
-          />
-          <View style={styles.line}/>
+    <View style={styles.mainComponent}>
+      <View>
+        <Text
+          style={[
+            atomicStyles.h4,
+            atomicStyles.bold,
+            styles.textStyle,
+            styles.header,
+          ]}>
+          {translate('homeScreen.upcoming')}
+        </Text>
       </View>
+      <FlatList
+        data={list}
+        renderItem={({item}) => (
+          <View
+            style={{alignItems: 'center', marginRight: SLIDER_WIDTH * 0.051}}>
+            <Image
+              style={{
+                width: SLIDER_WIDTH * 0.4,
+                height: SLIDER_HEIGHT * 0.32,
+                borderRadius: 22,
+              }}
+              source={{
+                uri: item.img,
+              }}
+            />
+            <Text
+              style={[
+                atomicStyles.h5,
+                atomicStyles.bold,
+                atomicStyles.textDark,
+                styles.textStyle,
+                atomicStyles.mt8px,
+              ]}>
+              {item.name}
+            </Text>
+            <Text
+              style={[
+                atomicStyles.h6,
+                atomicStyles.bold,
+                atomicStyles.textGray,
+                styles.textStyle,
+              ]}>
+              {item.release}
+            </Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
+      <View style={styles.line} />
+    </View>
   );
 };
 
@@ -91,5 +121,3 @@ UpcomingFilm.propTypes = {
 UpcomingFilm.displayName = nameof(UpcomingFilm);
 
 export default React.memo(UpcomingFilm);
-
-
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,8 @@
 import React, {FC, PropsWithChildren, ReactElement} from 'react';
 import nameof from 'ts-nameof.macro';
-// import styles from './HomeScreen.scss';
-import {useTranslation} from 'react-i18next';
-import {SafeAreaView, StatusBar, Text, View, ScrollView} from 'react-native';
+import styles from './HomeScreen.scss';
+// import {useTranslation} from 'react-i18next';
+import {SafeAreaView, StatusBar, ScrollView, View} from 'react-native';
 import MainTabBar from '../../components/organisms/MainTabBar/MainTabBar';
 import {StackScreenProps} from '@react-navigation/stack';
 import {atomicStyles, Colors} from 'src/styles';
@@ -21,17 +21,19 @@ const HomeScreen: FC<PropsWithChildren<HomeScreenProps>> = (
   props: PropsWithChildren<HomeScreenProps>,
 ): ReactElement => {
   const {navigation, route} = props;
-  const [translate] = useTranslation();
+  // const [translate] = useTranslation();
 
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor={Colors.Light_Gray} />
-      <SafeAreaView style={atomicStyles.container}>
+      <SafeAreaView style={[atomicStyles.container]}>
         <ScrollView>
-          <CategoryComponent/>  
-          <AvailableFilm/>
-          <UpcomingFilm/>
-          <FavoriteFilm/>
+          <View style={[styles.containerView]}>
+            <CategoryComponent />
+            <AvailableFilm />
+            <UpcomingFilm />
+            <FavoriteFilm />
+          </View>
         </ScrollView>
         <MainTabBar navigation={navigation} route={route} />
       </SafeAreaView>

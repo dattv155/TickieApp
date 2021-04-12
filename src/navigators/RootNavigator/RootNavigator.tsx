d8b30de9a6d69d82517bfd4b00de1d44b64f4b6e
Ngo Tien Tan <ngotientan811@gmail.com>
2021-04-12 13:34:10 +0700

    add authen package

--- src/navigators/RootNavigator/RootNavigator.tsx
+++ src/navigators/RootNavigator/RootNavigator.tsx
@@ -1,22 +1,13 @@
 import React, {FC, PropsWithChildren, ReactElement} from 'react';
 import nameof from 'ts-nameof.macro';
-import {
-  CardStyleInterpolators,
-  createStackNavigator,
-} from '@react-navigation/stack';
+import {CardStyleInterpolators, createStackNavigator,} from '@react-navigation/stack';
 import HomeScreen from '../../screens/HomeScreen/HomeScreen';
 import TabNavigator from '../TabNavigator/TabNavigator';
 import ProfilePage from 'src/screens/ProfilePage/ProfilePage';
 import NotificationScreen from 'src/screens/NotificationScreen/NotificationScreen';
-import LoginScreen from 'src/screens/LoginScreen/LoginScreen';
-import WelcomeScreen from 'src/screens/WelcomeScreen/WelcomeScreen';
-import ForgotPasswordScreen from 'src/screens/ForgotPasswordScreen/ForgotPasswordScreen';
-import VerifyCodeScreen from 'src/screens/VerifyCodeScreen/VerifyCodeScreen';
-import ChangePasswordScreen from 'src/screens/ChangePasswordScreen/ChangePasswordScreen';
 import AccountInfoScreen from 'src/screens/AccountInfoScreen/AccountInfoScreen';
 import ChangePasswordProfileScreen from 'src/screens/ChangePasswordProfileScreen/ChangePasswordProfileScreen';
 import MyTicketScreen from 'src/screens/MyTicketScreen/MyTicketScreen';
-// import {forNoAnimation} from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
 import {StackCardInterpolatedStyle} from '@react-navigation/stack/src/types';
 import DetailTicketScreen from 'src/screens/DetailTicketScreen/DetailTicketScreen';
 import GeneralSettingScreen from 'src/screens/GeneralSettingScreen/GeneralSettingScreen';
@@ -35,7 +26,7 @@ const RootNavigator: FC<
 > = (): ReactElement => {
   return (
     <Navigator
-      initialRouteName={WelcomeScreen.displayName}
+      initialRouteName={HomeScreen.displayName}
       headerMode="none"
       screenOptions={{
         cardOverlayEnabled: true,
@@ -67,31 +58,6 @@ const RootNavigator: FC<
         component={NotificationScreen}
         initialParams={{}}
         options={{cardStyleInterpolator: forNoAnimation}}
-      />
-      <Screen
-        name={WelcomeScreen.displayName}
-        component={WelcomeScreen}
-        initialParams={{}}
-      />
-      <Screen
-        name={LoginScreen.displayName}
-        component={LoginScreen}
-        initialParams={{}}
-      />
-      <Screen
-        name={ForgotPasswordScreen.displayName}
-        component={ForgotPasswordScreen}
-        initialParams={{}}
-      />
-      <Screen
-        name={VerifyCodeScreen.displayName}
-        component={VerifyCodeScreen}
-        initialParams={{}}
-      />
-      <Screen
-        name={ChangePasswordScreen.displayName}
-        component={ChangePasswordScreen}
-        initialParams={{}}
       />
       <Screen
         name={AccountInfoScreen.displayName}

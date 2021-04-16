eb86602be871d0ad00b2c2db2ae545cc0023913e
TrongDat <trongdat1505@gmail.com>
2021-04-16 23:40:56 +0700

    Fix ChooseSeatScreen

--- src/screens/ChooseSeatScreen/component/SmallTheater/SmallTheater.tsx
+++ src/screens/ChooseSeatScreen/component/SmallTheater/SmallTheater.tsx
@@ -2,9 +2,15 @@ import React, {FC, PropsWithChildren, ReactElement} from 'react';
 import nameof from 'ts-nameof.macro';
 import styles from './SmallTheater.scss';
 import Seat from 'src/screens/ChooseSeatScreen/component/Seat/Seat';
-import {View, Text, ListRenderItem, ListRenderItemInfo} from 'react-native';
+import {
+  View,
+  Text,
+  ListRenderItem,
+  ListRenderItemInfo,
+  FlatList,
+} from 'react-native';
 import {atomicStyles} from 'src/styles';
-import SvgIcon from 'src/components/atoms/SvgIcon/SvgIcon';
+import {SmallTheaterLayout} from 'src/sample/smallTheaterLayout';
 
 /**
  * File: SmallTheater.tsx
@@ -12,6 +18,9 @@ import SvgIcon from 'src/components/atoms/SvgIcon/SvgIcon';
  * @author TrongDat <trongdat1505@gmail.com>
  * @type {FC<PropsWithChildren<SmallTheaterProps>>}
  */
+
+const rowLabel = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
 const SmallTheater: FC<PropsWithChildren<SmallTheaterProps>> = (
   props: PropsWithChildren<SmallTheaterProps>,
 ): ReactElement => {
@@ -21,145 +30,90 @@ const SmallTheater: FC<PropsWithChildren<SmallTheaterProps>> = (
     },
     [],
   );
+
+  const renderLabelColumn: ListRenderItem<any> = React.useCallback(
+    ({item, index}: ListRenderItemInfo<any>) => {
+      return (
+        <Text
+          style={[atomicStyles.h7, atomicStyles.bold, styles.labelText]}
+          key={index}>
+          {item}
+        </Text>
+      );
+    },
+    [],
+  );
+
+  const renderLabelRow: ListRenderItem<any> = React.useCallback(
+    ({item, index}: ListRenderItemInfo<any>) => {
+      return (
+        <Text
+          style={[
+            atomicStyles.h7,
+            atomicStyles.bold,
+            styles.labelText,
+            styles.labelRow,
+          ]}
+          key={index}>
+          {item}
+        </Text>
+      );
+    },
+    [],
+  );
+
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.labelLeft}>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            A
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            B
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            C
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            D
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            E
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            F
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            G
-          </Text>
+    <View style={styles.container}>
+      <View style={styles.containerViewRow}>
+        <View style={styles.labelView}>
+          <FlatList
+            data={SmallTheaterLayout.map((item) => {
+              return item.title;
+            })}
+            renderItem={renderLabelColumn}
+            horizontal={true}
+            keyExtractor={(item, index) => item.toString() + index.toString()}
+            contentContainerStyle={styles.labelContainer}
+          />
         </View>
         <View style={styles.seatArea}>
-          <View style={styles.rowA}>
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-          </View>
-          <View style={styles.rowBF}>
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-          </View>
-          <View style={styles.rowBF}>
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-          </View>
-          <View style={styles.rowBF}>
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-          </View>
-          <View style={styles.rowBF}>
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-          </View>
-          <View style={styles.rowBF}>
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-            <Seat />
-          </View>
-          <View style={styles.rowBF}>
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-            <SvgIcon component={require('assets/icons/seat/CoupleSeat.svg')} />
-          </View>
+          {SmallTheaterLayout.map((row) => {
+            return (
+              <FlatList
+                data={row.data}
+                renderItem={renderItem}
+                horizontal={true}
+                keyExtractor={(item, index) =>
+                  item.toString() + index.toString()
+                }
+                contentContainerStyle={styles.rowStyle}
+              />
+            );
+          })}
         </View>
 
-        <View style={styles.labelRight}>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            A
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            B
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            C
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            D
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            E
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            F
-          </Text>
-          <Text style={[atomicStyles.h6, atomicStyles.bold, styles.labelText]}>
-            G
-          </Text>
+        <View style={styles.labelView}>
+          <FlatList
+            data={SmallTheaterLayout.map((item) => {
+              return item.title;
+            })}
+            renderItem={renderLabelColumn}
+            horizontal={true}
+            keyExtractor={(item, index) => item.toString() + index.toString()}
+            contentContainerStyle={styles.labelContainer}
+          />
         </View>
       </View>
-    </>
+      <View>
+        <FlatList
+          data={rowLabel}
+          renderItem={renderLabelRow}
+          horizontal={true}
+          keyExtractor={(item, index) => item.toString() + index.toString()}
+          contentContainerStyle={styles.labelRowContainer}
+        />
+      </View>
+    </View>
   );
 };
 

b36e2f4fa0d026839f90c5446ba64c15b9e93a5c
Huy-No1 <huygg12345@gmail.com>
2021-04-19 21:40:25 +0700

    1

--- src/components/HomeComponent/CategoryComponent/CategoryComponent.tsx
+++ src/components/HomeComponent/CategoryComponent/CategoryComponent.tsx
@@ -1,74 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import {View, Text, Dimensions, Image} from 'react-native';
-import Carousel from 'react-native-snap-carousel';
-
-import {scrollInterpolator, animatedStyles} from '../CategoryComponent/utils/animations';
-import styles from './CategoryComponent.scss';
-// import {atomicStyles} from '../../../styles';
-import { useTranslation } from 'react-i18next/';
-
-const SLIDER_WIDTH = Dimensions.get('window').width;
-const SLIDER_HEIGHT = Dimensions.get('window').height;
-const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.672);
-const ITEM_HEIGHT = Math.round(SLIDER_HEIGHT * 0.5);
-
-export default function CategoryComponent(props) {
-  const {list, display}= props;
-  const [carousel, setCarousel]= React.useState();
-  const [translate] = useTranslation();
-  const [width, setWidth]= React.useState(300);
-  const [index, setIndex]= React.useState(0);
-  const renderItem  = ({item}) => {
-    return (
-      <View>
-        <Image
-          style={[styles.imageContainer, 
-                  {
-                    width: ITEM_WIDTH,
-                    height: ITEM_HEIGHT
-                  }
-                ]}
-          source={{
-            uri: item.img
-          }}>
-        </Image>
-      </View>
-    );
-  }
-
-  return (
-    <View style={{display: display}}>
-      <Carousel
-        ref={(c) => (setCarousel(c))}
-        data={list}
-        renderItem={(item) => renderItem(item)}
-        sliderWidth={SLIDER_WIDTH}
-        itemWidth={ITEM_WIDTH}
-        containerCustomStyle={styles.carouselContainer}
-        // inactiveSlideShift={1}
-        onSnapToItem={index => setIndex(index)}
-        scrollInterpolator={scrollInterpolator}
-        slideInterpolatedStyle={animatedStyles}
-        useScrollView={true}
-      />
-      <View style={styles.info}>
-        <Text style={styles.headerText}>{list[index].name}</Text> 
-        <Text style={styles.release}>{translate("homeScreen.releaseDay") +": " + `${list[index].release}`}</Text>
-      </View>
-      <View style={styles.line}/>
-    </View>
-  );
-  
-}
-
-=======
 import React, {FC, PropsWithChildren, ReactElement} from 'react';
 import {
   View,
   Text,
   Dimensions,
-  StyleSheet,
   Image,
   TouchableOpacity,
 } from 'react-native';
@@ -112,7 +46,12 @@ const CategoryComponent: FC<PropsWithChildren<CategoryComponentProps>> = (
       <View>
         <TouchableOpacity onPress={handleGotoMovieScreen}>
           <Image
-            style={itemStyles.imageContainer}
+            style={[styles.imageContainer,
+                      {
+                        width: ITEM_WIDTH,
+                        height: ITEM_HEIGHT,
+                      }
+                  ]}
             source={{
               uri: item.img,
             }}
@@ -167,21 +106,6 @@ const CategoryComponent: FC<PropsWithChildren<CategoryComponentProps>> = (
   );
 };
 
-const itemStyles = StyleSheet.create({
-  imageContainer: {
-    width: ITEM_WIDTH,
-    height: ITEM_HEIGHT,
-    borderRadius: 39,
-    borderWidth: 3,
-  },
-  release: {
-    paddingTop: 3,
-    color: '#828282',
-    fontWeight: '500',
-    fontStyle: 'normal',
-  },
-});
-
 export interface CategoryComponentProps extends StackScreenProps<any> {
   //
   list?: any[];
@@ -198,4 +122,3 @@ CategoryComponent.propTypes = {
 CategoryComponent.displayName = nameof(CategoryComponent);
 
 export default React.memo(CategoryComponent);
->>>>>>> 071e4b1d12a9f9ace39136f95ad489fc03edd3ce

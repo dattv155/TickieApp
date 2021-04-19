e426b7e974db5b134f126d494082eabb7eadcd05
Huy-No1 <huygg12345@gmail.com>
2021-04-19 22:29:02 +0700

    11

---
+++ src/components/atoms/Notibox/Notibox.tsx
@@ -0,0 +1,119 @@
+import React, { FC, PropsWithChildren, ReactElement, useEffect } from 'react';
+import nameof from 'ts-nameof.macro';
+import styles from './Notibox.scss';
+import {View, Text, Animated, TouchableOpacity} from 'react-native';
+/**
+ * File: ./Notibox.tsx
+ * @created 2021-04-19 21:48:55
+ * @author Huy-No1 <huygg12345@gmail.com>
+ * @type {FC<PropsWithChildren<NotiboxProps>>}
+ */
+const Notibox: FC<PropsWithChildren<NotiboxProps>> = (
+  props: PropsWithChildren<NotiboxProps>,
+): ReactElement => {
+  const [height, setHeight]= React.useState(new Animated.Value(85));
+  const [title, setTitle]= React.useState("");
+  const {type, span, content} = props.data;
+
+  const press = ()=>{
+      Animated.timing(
+          height,
+          {
+              toValue: height._value == 85 ? 145: 85,
+              duration: 450,
+              useNativeDriver: false
+          }
+        ).start();
+
+  }
+  useEffect(() =>{
+      if(data === undefined) return ;
+      switch (type){
+          case "bookingsuccess": {
+              setTitle("Đặt vé thành công");
+              break;
+          }
+          case "discount": {
+              setTitle("Thông báo khuyến mãi");
+              break;
+          }
+          case "upcoming": {
+              setTitle("Phim sắp ra mắt");
+              break;
+          }
+          case "member": {
+              setTitle("Thông báo thành viên");
+              break;
+          }
+          case "newRelease": {
+              setTitle("Chính thức khởi chiếu");
+              break;
+          }
+          default:{
+              setTitle("Chung");
+          }
+      }
+  },[]);
+  return (
+      <TouchableOpacity style={{width: '100%', alignItems:'center'}} onPress={press}>
+      <Animated.View style={[
+          {
+              height: height
+          },
+          sstyle.box
+      ]}
+          >
+          <View style={styles.bigwrapper}>
+              <Text style={styles.type}>
+                  {title}
+              </Text>
+              <View style={styles.wrapper}>
+                  <Text style={styles.content}>
+                      {content}
+                  </Text>
+              </View> 
+              <Text style={styles.content}>
+                      {span}
+              </Text>
+          </View>
+      </Animated.View>
+      </TouchableOpacity>
+  )
+
+};
+
+const sstyle= StyleSheet.create({
+  box: {     
+    width: '90%',
+    backgroundColor: 'white',
+    borderRadius: 12,
+    paddingHorizontal: 13,
+    marginTop: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0.5,
+        height: 2.5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    paddingBottom: 9
+  },  
+})
+export interface NotiboxProps {
+  //
+  type?: string;
+  content?: string;
+  span?: string;
+}
+
+Notibox.defaultProps = {
+  //
+};
+
+Notibox.propTypes = {
+  //
+};
+
+Notibox.displayName = nameof(Notibox);
+
+export default React.memo(Notibox);
--- src/screens/NotificationScreen/NotificationScreen.tsx
+++ src/screens/NotificationScreen/NotificationScreen.tsx
@@ -1,11 +1,13 @@
-import React, {FC, PropsWithChildren, ReactElement} from 'react';
+import React, {FC, PropsWithChildren, ReactElement, useEffect} from 'react';
 import nameof from 'ts-nameof.macro';
 // import styles from './NotificationScreen.scss';
-import {SafeAreaView, Text, View} from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import MainTabBar from 'src/components/organisms/MainTabBar/MainTabBar';
 import {atomicStyles} from 'src/styles';
-
+import Notibox from '../../components/atoms/Notibox/Notibox';
+import firestore from '@react-native-firebase/firestore';
+import styles from './notification.scss';
 /**
  * File: NotificationScreen.tsx
  * @created 2021-03-09 17:09:49
@@ -16,24 +18,60 @@ const NotificationScreen: FC<PropsWithChildren<NotificationScreenProps>> = (
   props: PropsWithChildren<NotificationScreenProps>,
 ): ReactElement => {
   const {navigation, route} = props;
+  const db= firestore();
+  const [list, setList]= React.useState([]);
+  const userId= "9AKYA27yIDfm9UrJ65InchSZ62H2";
+
+  useEffect(() =>{
+    async function fetchData(){
+      var exp=[];
+      var dataGeneral= await db.collection("notification").doc("general").collection("1").orderBy("day", "desc").get();
+      dataGeneral.forEach(item => exp.push(item.data()));
+      var dataSpecific= await db.collection("notification").doc("specific").collection("1").where("userId", "==", userId).get();
+      dataSpecific.forEach(item => exp.push(item.data()));
+      exp.sort((a, b) => a.day.seconds < b.day.seconds ? 1 : (a.day.seconds > b.day.seconds ? -1: 0));
+      setList(exp);
+    }
+    fetchData();
+  },[]); 
+  const renderData = ()=>{
+    let item=[];
+    var day=0;
+    var isoday;
+    for(let i=0; i<list.length; i++){
+      if(list[i].day.seconds * 1000 > Date.now()) continue;
+      if(list[i].day.seconds * 1000 != day){
+        day=list[i].day.seconds * 1000;
+        isoday= new Date(day);
+        let realday;
+        if(isoday.toLocaleDateString() === new Date().toLocaleDateString())
+            realday="Hôm nay";
+
+        else if(isoday.toLocaleDateString() === new Date(Date.now() - 86400000).toLocaleDateString())
+            realday="Hôm qua";
+        else
+            realday= `${isoday.getDate()}/${isoday.getMonth() + 1}/${isoday.getFullYear()}`;
+        item.push(<Text key={day} style={[styles.day, atomicStyles.regular]}>{realday}</Text>);
+      }
+
+      item.push(<Notibox key={i} data={list[i]}/>);
+    }
+    return item;
+  }
+
   return (
-    <SafeAreaView
-      style={[
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
         {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        },
-        atomicStyles.container,
-      ]}>
-      <View>
-        <Text>Notification Page</Text>
-      </View>
+        renderData()
+        }
+        <View style={styles.padding}/>
+      </ScrollView>
+      
       <MainTabBar navigation={navigation} route={route} />
     </SafeAreaView>
   );
-};
-
+}
 export interface NotificationScreenProps extends StackScreenProps<any> {
   //
 }
@@ -44,6 +82,8 @@ NotificationScreen.defaultProps = {
 
 NotificationScreen.propTypes = {
   //
+  exp: 'string';
+  
 };
 
 NotificationScreen.displayName = nameof(NotificationScreen);

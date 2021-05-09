c0e6e7766a4157dd245640ace01dfff58e9f6dab
Ngo Tien Tan <ngotientan811@gmail.com>
2021-05-09 10:51:31 +0700

    first test momo payment

---
+++ src/models/MomoData.ts
@@ -0,0 +1,21 @@
+import {Model} from '@react3l/react3l';
+
+export class MomoData extends Model {
+  public action?: string;
+  public partner?: string;
+  public appScheme?: string;
+  public amount?: number;
+  public description?: string;
+  public merchantcode?: string;
+  public merchantname?: string;
+  public merchantnamelabel?: string;
+  public language?: string;
+  public fee?: number;
+  public username?: string;
+  public orderId?: string;
+  public requestId?: string;
+  public orderLabel?: string;
+  public extra?: string;
+  public enviroment?: '0' | '1';
+  public isDev?: boolean;
+}

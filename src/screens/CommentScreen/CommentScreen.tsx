a00ae5539a6fee917e878728b836347b0125d332
TrongDat <trongdat1505@gmail.com>
2021-05-12 22:10:53 +0700

    Merge comment feature

--- src/screens/CommentScreen/CommentScreen.tsx
+++ src/screens/CommentScreen/CommentScreen.tsx
@@ -1,21 +1,14 @@
-import React, { FC, PropsWithChildren, ReactElement, useEffect } from 'react';
+import React, {FC, PropsWithChildren, ReactElement, useEffect} from 'react';
 import nameof from 'ts-nameof.macro';
-import styles from'./CommentScreen.scss';
-import {
-  Image,
-  Text,
-  View,
-
-  TouchableOpacity,
-  TextInput
-} from 'react-native';
+import styles from './CommentScreen.scss';
+import {Image, Text, View, TouchableOpacity, TextInput} from 'react-native';
 
 import {atomicStyles} from 'src/styles';
 
 import SvgIcon from 'src/components/atoms/SvgIcon/SvgIcon';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
-import { any } from 'prop-types';
+import {StackScreenProps} from '@react-navigation/stack';
 
 /**
  * File: CommentScreen.tsx
@@ -27,89 +20,108 @@ const CommentScreen: FC<PropsWithChildren<CommentScreenProps>> = (
   props: PropsWithChildren<CommentScreenProps>,
 ): ReactElement => {
   const {navigation, route} = props;
-  const userId=auth().currentUser.uid;
-  const db=firestore();
-  const yellowstar= require('assets/icons/star.svg');
-  const graystar= require('assets/icons/stargray.svg');
-  const [svg,setSvg]= React.useState([yellowstar, graystar, graystar, graystar, graystar]);
-  const [text, onChangeText] = React.useState("");
-  const [curStar, setCurStar]= React.useState(1);
-  const handleComment = () =>{
-      console.log("add comment");
-  }
-  useEffect(() =>{
-    var exp=[...svg];
-    for(let i=0; i<curStar; i++)
-      exp[i]=yellowstar;
-    for(let i=curStar; i<5; i++)
-      exp[i]=graystar;
+  const userId = auth().currentUser.uid;
+  const db = firestore();
+  const yellowstar = require('assets/icons/star.svg');
+  const graystar = require('assets/icons/stargray.svg');
+  const [svg, setSvg] = React.useState([
+    yellowstar,
+    graystar,
+    graystar,
+    graystar,
+    graystar,
+  ]);
+  const [text, onChangeText] = React.useState('');
+  const [curStar, setCurStar] = React.useState(1);
+  const handleComment = () => {
+    console.log('add comment');
+  };
+  useEffect(() => {
+    let exp = [...svg];
+    for (let i = 0; i < curStar; i++) {
+      exp[i] = yellowstar;
+    }
+    for (let i = curStar; i < 5; i++) {
+      exp[i] = graystar;
+    }
     setSvg(exp);
-  },[curStar])
-  return (
+  }, [curStar, graystar, svg, yellowstar]);
 
+  return (
     <View style={styles.yourcomment}>
-      <TouchableOpacity style={styles.goback} >
-        <SvgIcon component={require('assets/icons/backIconRound.svg')}/> 
+      <TouchableOpacity style={styles.goback}>
+        <SvgIcon component={require('assets/icons/backIconRound.svg')} />
       </TouchableOpacity>
 
-    <Text style={[styles.textyourcomment,atomicStyles.bold]}>Đánh giá của bạn</Text>
-    <View style={styles.header}>
-      <Image
-        source={require('assets/images/mulan-poster.png')}
-        resizeMode="cover"
-        style={styles.imageheaderinfo}
-      />
-      <View style={styles.headerinfo}>
-        <Text style={[styles.headerinfo_name,atomicStyles.bold]}>Mulan</Text>
-        <Text style={[styles.headerinfo_release, atomicStyles.regular]}>2020</Text>
-        <Text style={[styles.headerinfo_kind, atomicStyles.regular]}>PG 13, hành động, cổ trang</Text>
-      </View> 
-    </View>
-    <View style={styles.ratemovie}>
-        <Text style={[styles.rateheader, atomicStyles.bold]}>Xếp hạng phim</Text>
+      <Text style={[styles.textyourcomment, atomicStyles.bold]}>
+        Đánh giá của bạn
+      </Text>
+      <View style={styles.header}>
+        <Image
+          source={require('assets/images/mulan-poster.png')}
+          resizeMode="cover"
+          style={styles.imageheaderinfo}
+        />
+        <View style={styles.headerinfo}>
+          <Text style={[styles.headerinfo_name, atomicStyles.bold]}>Mulan</Text>
+          <Text style={[styles.headerinfo_release, atomicStyles.regular]}>
+            2020
+          </Text>
+          <Text style={[styles.headerinfo_kind, atomicStyles.regular]}>
+            PG 13, hành động, cổ trang
+          </Text>
+        </View>
+      </View>
+      <View style={styles.ratemovie}>
+        <Text style={[styles.rateheader, atomicStyles.bold]}>
+          Xếp hạng phim
+        </Text>
         <View style={styles.pressstar}>
-          <TouchableOpacity style={styles.star} onPress={() => setCurStar(1)}>          
-            <SvgIcon component={svg[0]} scaleY={1.4} scaleX={1.4}  />
+          <TouchableOpacity style={styles.star} onPress={() => setCurStar(1)}>
+            <SvgIcon component={svg[0]} scaleY={1.4} scaleX={1.4} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.star} onPress={() => setCurStar(2)}>          
-            <SvgIcon component={svg[1]} scaleY={1.4} scaleX={1.4}  />
+          <TouchableOpacity style={styles.star} onPress={() => setCurStar(2)}>
+            <SvgIcon component={svg[1]} scaleY={1.4} scaleX={1.4} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.star} onPress={() => setCurStar(3)}>          
-            <SvgIcon component={svg[2]} scaleY={1.4} scaleX={1.4}  />
+          <TouchableOpacity style={styles.star} onPress={() => setCurStar(3)}>
+            <SvgIcon component={svg[2]} scaleY={1.4} scaleX={1.4} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.star} onPress={() => setCurStar(4)}>          
-            <SvgIcon component={svg[3]} scaleY={1.4} scaleX={1.4}  />
+          <TouchableOpacity style={styles.star} onPress={() => setCurStar(4)}>
+            <SvgIcon component={svg[3]} scaleY={1.4} scaleX={1.4} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.star} onPress={() => setCurStar(5)}>          
-            <SvgIcon component={svg[4]} scaleY={1.4} scaleX={1.4}  />
+          <TouchableOpacity style={styles.star} onPress={() => setCurStar(5)}>
+            <SvgIcon component={svg[4]} scaleY={1.4} scaleX={1.4} />
           </TouchableOpacity>
         </View>
-        
-       <TextInput
-          style={[styles.input,atomicStyles.regular]}
+
+        <TextInput
+          style={[styles.input, atomicStyles.regular]}
           placeholder="Để lại đánh giá của bạn tại đây..."
           multiline={true}
-          textAlignVertical={"top"}
+          textAlignVertical={'top'}
           onChangeText={onChangeText}
           value={text}
         />
-        <TouchableOpacity style={[styles.buttondanhgia]} onPress={handleComment}>
-          <View >
-            <Text style={[styles.textbuttondanhgia,atomicStyles.regular]}>Gửi đánh giá</Text>
+        <TouchableOpacity
+          style={[styles.buttondanhgia]}
+          onPress={handleComment}>
+          <View>
+            <Text style={[styles.textbuttondanhgia, atomicStyles.regular]}>
+              Gửi đánh giá
+            </Text>
           </View>
-        </TouchableOpacity> 
-    </View> 
-  </View>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
-export interface CommentScreenProps {
+export interface CommentScreenProps extends StackScreenProps<any> {
   //
   auth?: any;
 }
 
 CommentScreen.defaultProps = {
-
   //
 };
 
--- src/screens/MovieInfoScreen/component/FilmRate/FilmRate.tsx
+++ src/screens/MovieInfoScreen/component/FilmRate/FilmRate.tsx
@@ -1,17 +1,8 @@
-import React, { FC, PropsWithChildren, ReactElement, useEffect } from 'react';
+import React, {FC, PropsWithChildren, ReactElement, useEffect} from 'react';
 import nameof from 'ts-nameof.macro';
-import styles from'./FilmRate.scss';
-import {
-  Image,
-  ScrollView,
-  Text,
-  View,
-  StatusBar,
-  ListRenderItem,
-  ListRenderItemInfo,
-  FlatList,
-  TouchableOpacity,
-} from 'react-native';
+import styles from './FilmRate.scss';
+import {Text, View} from 'react-native';
+import {Obj} from 'src/screens/MovieInfoScreen/MovieInfoScreen';
 
 /**
  * File: FilmRate.tsx
@@ -22,48 +13,53 @@ import {
 const FilmRate: FC<PropsWithChildren<FilmRateProps>> = (
   props: PropsWithChildren<FilmRateProps>,
 ): ReactElement => {
-  const {data}= props;
-  const [rate,setRate]=React.useState([0,0,0,0,0]);
-  useEffect(()=> {
-    var leng= data.length;
-    if(leng === 0) return;
-    else {
-      var exp=[...rate];
-      exp=exp.map((item, index) => data.filter(items => items.data().rate === (5 - index)).length/leng * 100);
+  const {data} = props;
+  const [rate, setRate] = React.useState<number[]>([0, 0, 0, 0, 0]);
+  useEffect(() => {
+    const leng = data.length;
+    if (leng === 0) {
+      return;
+    } else {
+      let exp = [...rate];
+      exp = exp.map(
+        (item, index) =>
+          (data.filter((items: Obj) => items.rate === 5 - index).length /
+            leng) *
+          100,
+      );
       setRate(exp);
     }
-  },[data])
+  }, [data, rate]);
   return (
     <View style={styles.fiveline}>
       <View style={styles.tv}>
         <Text style={styles.tvtext}>Tuyệt vời</Text>
         <View style={styles.outline}>
-          <View style={[styles.inline5,{width: `${rate[0]}%`}]}/>
-
+          <View style={[styles.inline5, {width: `${rate[0]}%`}]} />
         </View>
       </View>
       <View style={styles.tv}>
         <Text style={styles.tvtext}>Tốt</Text>
         <View style={styles.outline}>
-        <View style={[styles.inline4,{width: `${rate[1]}%`}]}/>
+          <View style={[styles.inline4, {width: `${rate[1]}%`}]} />
         </View>
       </View>
       <View style={styles.tv}>
         <Text style={styles.tvtext}>Khá</Text>
         <View style={styles.outline}>
-        <View style={[styles.inline3,{width: `${rate[2]}%`}]}/>
+          <View style={[styles.inline3, {width: `${rate[2]}%`}]} />
         </View>
       </View>
       <View style={styles.tv}>
         <Text style={styles.tvtext}>Trung bình</Text>
         <View style={styles.outline}>
-        <View style={[styles.inline2,{width: `${rate[3]}%`}]}/>
+          <View style={[styles.inline2, {width: `${rate[3]}%`}]} />
         </View>
       </View>
       <View style={styles.tv}>
         <Text style={styles.tvtext}>Dở</Text>
         <View style={styles.outline}>
-        <View style={[styles.inline1,{width: `${rate[4]}%`}]}/>
+          <View style={[styles.inline1, {width: `${rate[4]}%`}]} />
         </View>
       </View>
     </View>
@@ -72,6 +68,7 @@ const FilmRate: FC<PropsWithChildren<FilmRateProps>> = (
 
 export interface FilmRateProps {
   //
+  data?: any;
 }
 
 FilmRate.defaultProps = {

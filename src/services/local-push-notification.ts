6ee7235af685e339aba5c3f65e94ad9b289747d3
Ngo Tien Tan <ngotientan811@gmail.com>
2021-05-17 13:27:36 +0700

    fix local notification

--- src/services/local-push-notification.ts
+++ src/services/local-push-notification.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import PushNotification from 'react-native-push-notification';
+import PushNotification, {Importance} from 'react-native-push-notification';
 
 PushNotification.configure({
   // (required) Called when a remote or local notification is opened or received
@@ -10,8 +10,22 @@ PushNotification.configure({
   requestPermissions: true,
 });
 
+PushNotification.createChannel(
+  {
+    channelId: 'channel-id', // (required)
+    channelName: 'My channel', // (required)
+    channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
+    playSound: false, // (optional) default: true
+    soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+    importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+  },
+  (created: any) => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+);
+
 export const LocalNotification = (movie: string) => {
   PushNotification.localNotification({
+    channelId: 'channel-id',
     autoCancel: true,
     largeIcon: 'ic_launcher', // (optional) default: "ic_launcher". Use "" for no large icon.
     largeIconUrl:

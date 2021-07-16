c6203f3b55ed6fb3eb450a3eb1c46729a2926e6d
Ngo Tien Tan <ngotientan811@gmail.com>
2021-07-17 00:00:50 +0700

    sua thanh toan momo

--- src/services/momo-service/use-payment.ts
+++ src/services/momo-service/use-payment.ts
@@ -6,23 +6,13 @@ import {MomoData} from 'src/models/MomoData';
 import {NativeEventEmitter, NativeModules, Platform} from 'react-native';
 // @ts-ignore
 import RNMomosdk from 'react-native-momosdk';
-import {globalState} from 'src/app/global-state';
 
 const RNMoMoPaymentModule = NativeModules.RNMomosdk;
 const EventEmitter = new NativeEventEmitter(RNMoMoPaymentModule);
 
-export function usePayment(): [
-  (value: number) => void,
-  () => void,
-  (value: Payment) => void,
-  string,
-] {
-  const [bookingData] = globalState.useBookingData();
-
-  const [amount, setAmount] = React.useState<number>(0);
-
-  const enviroment = '0'; //"1": production
-
+export function usePayment(
+  amount: number,
+): [(amount: number, movie: string) => void, (value: Payment) => void, string] {
   const [paymentResponse, setPaymentResponse] = React.useState<string>('');
 
   const [payment, setPayment] = React.useState<Payment>({
@@ -31,10 +21,6 @@ export function usePayment(): [
     description: '',
     processing: false,
   });
-
-  const handleChangeAmount = React.useCallback((value: number) => {
-    setAmount(value);
-  }, []);
 
   const handleChangePayment = React.useCallback((value: Payment) => {
     setPayment({
@@ -65,37 +51,35 @@ export function usePayment(): [
     },
   );
 
-  const handleSendRequest = React.useCallback(async () => {
-    if (!payment.processing) {
-      let jsonData = new MomoData();
-      jsonData.enviroment = enviroment; //"0": SANBOX , "1": PRODUCTION
-      jsonData.action = 'gettoken';
-      jsonData.isDev = true; //SANBOX only , remove this key on PRODUCTION
-      jsonData.merchantname = 'Tickie';
-      jsonData.merchantcode = 'TICKIE01';
-      jsonData.merchantnamelabel = 'Nhà cung cấp';
-      jsonData.description = bookingData.movieName;
-      jsonData.amount = amount;
-      jsonData.orderId = bookingData.bookingId;
-      jsonData.requestId = 'CykaBlyat';
-      jsonData.orderLabel = 'Order Code';
-      jsonData.appScheme = 'momocgv20170101'; // iOS App Only , get from Info.plist > key URL types > URL Schemes. Check Readme
-      if (Platform.OS === 'android') {
-        let dataPayment = await RNMomosdk.requestPayment(jsonData);
-        await momoHandleResponse(dataPayment);
+  const handleSendRequest = React.useCallback(
+    async (amount: number, movie: string) => {
+      if (!payment.processing) {
+        let jsonData = new MomoData();
+        jsonData.enviroment = '0'; //"0": SANBOX , "1": PRODUCTION
+        jsonData.action = 'gettoken';
+        jsonData.isDev = true; //SANBOX only , remove this key on PRODUCTION
+        jsonData.merchantname = 'Cinemas';
+        jsonData.merchantcode = 'CGV01';
+        jsonData.merchantnamelabel = 'Nhà cung cấp';
+        jsonData.description = movie;
+        jsonData.amount = amount;
+        jsonData.orderId = 'bill234284290348';
+        jsonData.requestId = 'your_requestId';
+        jsonData.orderLabel = 'Ma don hang';
+        jsonData.appScheme = 'momocgv20170101'; // iOS App Only , get from Info.plist > key URL types > URL Schemes. Check Readme
+        if (Platform.OS === 'android') {
+          let dataPayment = await RNMomosdk.requestPayment(jsonData);
+          await momoHandleResponse(dataPayment);
+        } else {
+          RNMomosdk.requestPayment(JSON.stringify(jsonData));
+        }
+        setPayment({description: '', processing: true});
       } else {
-        RNMomosdk.requestPayment(JSON.stringify(jsonData));
+        setPayment({description: '.....', processing: false});
       }
-      setPayment({description: '', processing: true});
-    } else {
-      setPayment({description: '.....', processing: false});
-    }
-  }, [
-    amount,
-    bookingData.bookingId,
-    bookingData.movieName,
-    payment.processing,
-  ]);
+    },
+    [payment.processing],
+  );
 
   const momoHandleResponse = async (response: any) => {
     try {
@@ -116,10 +100,5 @@ export function usePayment(): [
     }
   };
 
-  return [
-    handleChangeAmount,
-    handleSendRequest,
-    handleChangePayment,
-    paymentResponse,
-  ];
+  return [handleSendRequest, handleChangePayment, paymentResponse];
 }

b09f3a330e4b5887b1bbd670591c8d620c18cf7a
Ngo Tien Tan <ngotientan811@gmail.com>
2021-07-17 00:50:48 +0700

    thay bottom sheet bằng react native modal

--- src/screens/AccountInfoScreen/AccountInfoScreen.tsx
+++ src/screens/AccountInfoScreen/AccountInfoScreen.tsx
@@ -16,8 +16,6 @@ import DatePicker from 'react-native-date-picker';
 import SvgIcon from 'src/components/atoms/SvgIcon/SvgIcon';
 import Dash from 'react-native-dash';
 import {Provinces} from 'src/sample/provinces';
-import Animated from 'react-native-reanimated';
-import BottomSheet from 'reanimated-bottom-sheet';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import Toast from 'react-native-simple-toast';
@@ -26,6 +24,7 @@ import moment from 'moment';
 import AccountInfoSkeleton from 'src/screens/AccountInfoScreen/AccountInfoSkeleton/AccountInfoSkeleton';
 import {useTranslation} from 'react-i18next/';
 import {showInfo} from 'src/helpers/toast';
+import ModalComponent from 'src/screens/AccountInfoScreen/components/ModalComponent/ModalComponent';
 
 /**
  * File: AccountInfoScreen.tsx
@@ -58,7 +57,10 @@ const AccountInfoScreen: FC<PropsWithChildren<AccountInfoScreenProps>> = (
     loading,
   ] = getAccount.getAccountInfo(navigation);
 
+  const [saveLoading, setSaveLoading] = React.useState<boolean>(false);
+
   const updateProfile = React.useCallback(() => {
+    setSaveLoading(true);
     firestore()
       .collection('users')
       .doc(auth().currentUser.uid)
@@ -71,6 +73,7 @@ const AccountInfoScreen: FC<PropsWithChildren<AccountInfoScreenProps>> = (
         dateOfBirth: firestore.Timestamp.fromDate(dateOfBirth),
       })
       .then(() => {
+        setSaveLoading(false);
         showInfo(translate('accountInfo.saveSuccess'));
       })
       .catch((e) => {
@@ -78,301 +81,268 @@ const AccountInfoScreen: FC<PropsWithChildren<AccountInfoScreenProps>> = (
       });
   }, [dateOfBirth, email, fullname, gender, phoneNumber, province, translate]);
 
-  const sheetRef = React.useRef(null);
-  const provinceRef = React.useRef(null);
-  const genderRef = React.useRef(null);
-  const fall = new Animated.Value<number>(1);
+  const [isDatePickVisible, setDatePickVisible] = React.useState<boolean>(
+    false,
+  );
 
-  const renderContent = () => (
-    <SafeAreaView style={styles.bottomBoxContainer}>
-      <View style={{alignItems: 'center'}}>
-        <View style={styles.swipeDown} />
-        <DatePicker
-          style={[
-            {
-              height: 150,
-              width: 350,
-            },
-          ]}
-          date={dateOfBirth}
-          onDateChange={handleChangeDateOfBirth}
-          mode={'date'}
-        />
-      </View>
-    </SafeAreaView>
+  const [isGenderPickVisible, setGenderPickVisible] = React.useState<boolean>(
+    false,
+  );
+
+  const [
+    isProvincePickVisible,
+    setProvincePickVisible,
+  ] = React.useState<boolean>(false);
+
+  const handleOpenDatePick = React.useCallback(() => {
+    setDatePickVisible(true);
+  }, []);
+
+  const handleCloseDatePick = React.useCallback(() => {
+    setDatePickVisible(false);
+  }, []);
+
+  const handleOpenGenderPick = React.useCallback(() => {
+    setGenderPickVisible(true);
+  }, []);
+
+  const handleCloseGenderPick = React.useCallback(() => {
+    setGenderPickVisible(false);
+  }, []);
+
+  const handleOpenProvincePick = React.useCallback(() => {
+    setProvincePickVisible(true);
+  }, []);
+
+  const handleCloseProvincePick = React.useCallback(() => {
+    setProvincePickVisible(false);
+  }, []);
+
+  const datePick = () => (
+    <DatePicker
+      style={styles.pickerStyle}
+      date={dateOfBirth}
+      onDateChange={handleChangeDateOfBirth}
+      mode={'date'}
+    />
   );
 
   const provincePick = () => (
-    <SafeAreaView style={styles.bottomBoxContainer}>
-      <View style={{alignItems: 'center'}}>
-        <View style={styles.swipeDown} />
-        <Picker
-          style={{backgroundColor: 'white', width: 300, height: 150}}
-          selectedValue={province}
-          pickerData={Provinces.map((province: any) => province.name)}
-          onValueChange={handleChangeProvince}
-        />
-      </View>
-    </SafeAreaView>
+    <Picker
+      style={styles.pickerStyle}
+      selectedValue={province}
+      pickerData={Provinces.map((province: any) => province.name)}
+      onValueChange={handleChangeProvince}
+    />
   );
 
   const genderPick = () => (
-    <SafeAreaView style={styles.bottomBoxContainer}>
-      <View style={{alignItems: 'center'}}>
-        <View style={styles.swipeDown} />
-        <Picker
-          style={{
-            backgroundColor: 'white',
-            width: 300,
-            height: 150,
-          }}
-          selectedValue={gender}
-          pickerData={['Nam', 'Nữ', 'Khác']}
-          onValueChange={handleChangeGender}
-        />
-      </View>
-    </SafeAreaView>
+    <Picker
+      style={styles.pickerStyle}
+      selectedValue={gender}
+      pickerData={['Nam', 'Nữ', 'Khác']}
+      onValueChange={handleChangeGender}
+    />
   );
 
   const handleGoToChangePasswordProfileScreen = React.useCallback(() => {
     navigation.navigate(ChangePasswordProfileScreen.displayName);
   }, [navigation]);
 
-  const handleOpenBottomSheet = React.useCallback(() => {
-    sheetRef.current.snapTo(0);
-    provinceRef.current.snapTo(1);
-    genderRef.current.snapTo(1);
-  }, []);
-
-  const handleCloseBottomSheet = React.useCallback(() => {
-    sheetRef.current.snapTo(1);
-    provinceRef.current.snapTo(1);
-    genderRef.current.snapTo(1);
-  }, []);
-
-  const handleOpenProvinceChoice = React.useCallback(() => {
-    provinceRef.current.snapTo(0);
-    sheetRef.current.snapTo(1);
-    genderRef.current.snapTo(1);
-  }, []);
-
-  const handleOpenGenderChoice = React.useCallback(() => {
-    genderRef.current.snapTo(0);
-    sheetRef.current.snapTo(1);
-    provinceRef.current.snapTo(1);
-  }, []);
-
   return (
     <>
-      <BottomSheet
-        ref={sheetRef}
-        snapPoints={[200, 0, 0]}
-        initialSnap={1}
-        renderHeader={renderContent}
-        callbackNode={fall}
-        enabledGestureInteraction={true}
-        enabledContentTapInteraction={false}
-      />
+      <ModalComponent
+        onBackdropPress={handleCloseDatePick}
+        isVisible={isDatePickVisible}>
+        {datePick()}
+      </ModalComponent>
 
-      <BottomSheet
-        ref={provinceRef}
-        snapPoints={[200, 0, 0]}
-        initialSnap={1}
-        renderHeader={provincePick}
-        callbackNode={fall}
-        enabledGestureInteraction={true}
-        enabledContentTapInteraction={false}
-      />
+      <ModalComponent
+        onBackdropPress={handleCloseGenderPick}
+        isVisible={isGenderPickVisible}>
+        {genderPick()}
+      </ModalComponent>
 
-      <BottomSheet
-        ref={genderRef}
-        snapPoints={[200, 0, 0]}
-        initialSnap={1}
-        renderHeader={genderPick}
-        callbackNode={fall}
-        enabledGestureInteraction={true}
-        enabledContentTapInteraction={false}
-      />
-      <Pressable onPress={handleCloseBottomSheet}>
-        <Animated.View
-          style={{
-            opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)),
-          }}>
-          <DefaultLayout
-            navigation={navigation}
-            route={route}
-            left="back-button"
-            right={<HeaderIconPlaceholder />}
-            title={
-              <Text
-                style={[
-                  atomicStyles.h3,
-                  atomicStyles.bold,
-                  styles.textStyle,
-                  atomicStyles.mt16px,
-                ]}>
-                {translate('accountInfo.header')}
-              </Text>
-            }
-            gradient={false}
-            customHeader={false}>
-            <SafeAreaView style={styles.screenContainer}>
-              <View style={styles.viewContainer}>
-                {loading ? (
-                  <AccountInfoSkeleton />
-                ) : (
-                  <InputProfile
-                    label={translate('accountInfo.name')}
-                    keyboardType="default"
-                    placeholder={fullname}
-                    onChangeText={handleChangeFullname}
-                  />
-                )}
+      <ModalComponent
+        onBackdropPress={handleCloseProvincePick}
+        isVisible={isProvincePickVisible}>
+        {provincePick()}
+      </ModalComponent>
 
-                {loading ? (
-                  <AccountInfoSkeleton />
-                ) : (
-                  <InputProfile
-                    label={translate('accountInfo.phoneNumber')}
-                    keyboardType="number-pad"
-                    placeholder={phoneNumber}
-                    onChangeText={handleChangePhoneNumber}
-                  />
-                )}
+      <DefaultLayout
+        navigation={navigation}
+        route={route}
+        left="back-button"
+        right={<HeaderIconPlaceholder />}
+        title={
+          <Text
+            style={[
+              atomicStyles.h3,
+              atomicStyles.bold,
+              styles.textStyle,
+              atomicStyles.mt16px,
+            ]}>
+            {translate('accountInfo.header')}
+          </Text>
+        }
+        gradient={false}
+        customHeader={false}>
+        <SafeAreaView style={styles.screenContainer}>
+          <View style={styles.viewContainer}>
+            {loading ? (
+              <AccountInfoSkeleton />
+            ) : (
+              <InputProfile
+                label={translate('accountInfo.name')}
+                keyboardType="default"
+                placeholder={fullname}
+                onChangeText={handleChangeFullname}
+              />
+            )}
 
-                {loading ? (
-                  <AccountInfoSkeleton />
-                ) : (
-                  <InputProfile
-                    label={translate('accountInfo.email')}
-                    keyboardType="email-address"
-                    placeholder={email}
-                    onChangeText={handleChangeEmail}
-                  />
-                )}
+            {loading ? (
+              <AccountInfoSkeleton />
+            ) : (
+              <InputProfile
+                label={translate('accountInfo.phoneNumber')}
+                keyboardType="number-pad"
+                placeholder={phoneNumber}
+                onChangeText={handleChangePhoneNumber}
+              />
+            )}
 
-                {loading ? (
-                  <AccountInfoSkeleton />
-                ) : (
-                  <View style={styles.dateSexSection}>
-                    <View style={[atomicStyles.w50]}>
-                      <Text
-                        style={[
-                          atomicStyles.h5,
-                          atomicStyles.bold,
-                          styles.textStyle,
-                          atomicStyles.mb8px,
-                        ]}>
-                        {translate('accountInfo.dateOfBirth')}
-                      </Text>
-                      <Pressable onPress={handleOpenBottomSheet}>
-                        <Text
-                          style={[
-                            atomicStyles.h5,
-                            atomicStyles.bold,
-                            styles.textStyle,
-                            atomicStyles.textGray,
-                            atomicStyles.mb16px,
-                            atomicStyles.mt8px,
-                          ]}>
-                          {moment(dateOfBirth).format('DD/MM/YYYY')}
-                        </Text>
-                      </Pressable>
-                    </View>
-                    <SvgIcon
-                      component={require('assets/icons/LineHorizontal.svg')}
-                    />
-                    <View style={[atomicStyles.w100]}>
-                      <Text
-                        style={[
-                          atomicStyles.w50,
-                          atomicStyles.textRight,
-                          atomicStyles.h5,
-                          atomicStyles.bold,
-                          styles.textStyle,
-                          atomicStyles.mb4px,
-                        ]}>
-                        {translate('accountInfo.gender')}
-                      </Text>
+            {loading ? (
+              <AccountInfoSkeleton />
+            ) : (
+              <InputProfile
+                label={translate('accountInfo.email')}
+                keyboardType="email-address"
+                placeholder={email}
+                onChangeText={handleChangeEmail}
+              />
+            )}
 
-                      <Pressable onPress={handleOpenGenderChoice}>
-                        <Text
-                          style={[
-                            atomicStyles.h5,
-                            atomicStyles.bold,
-                            atomicStyles.textRight,
-                            styles.textStyle,
-                            atomicStyles.textGray,
-                            atomicStyles.mb16px,
-                            atomicStyles.mt8px,
-                            atomicStyles.w50,
-                          ]}>
-                          {gender}
-                        </Text>
-                      </Pressable>
-                    </View>
-                  </View>
-                )}
-
-                {loading ? (
-                  <AccountInfoSkeleton />
-                ) : (
-                  <>
-                    <Dash
-                      dashGap={0}
-                      dashLength={3}
-                      dashThickness={1}
-                      style={[styles.dash, atomicStyles.mb16px]}
-                      dashColor={Colors.Gray}
-                    />
-
-                    <View
+            {loading ? (
+              <AccountInfoSkeleton />
+            ) : (
+              <View style={styles.dateSexSection}>
+                <View style={[atomicStyles.w50]}>
+                  <Text
+                    style={[
+                      atomicStyles.h5,
+                      atomicStyles.bold,
+                      styles.textStyle,
+                      atomicStyles.mb8px,
+                    ]}>
+                    {translate('accountInfo.dateOfBirth')}
+                  </Text>
+                  <Pressable onPress={handleOpenDatePick}>
+                    <Text
                       style={[
-                        atomicStyles.flexRow,
-                        atomicStyles.justifyContentBetween,
+                        atomicStyles.h5,
+                        atomicStyles.bold,
+                        styles.textStyle,
+                        atomicStyles.textGray,
+                        atomicStyles.mb16px,
                         atomicStyles.mt8px,
                       ]}>
-                      <Text
-                        style={[
-                          atomicStyles.h5,
-                          atomicStyles.bold,
-                          styles.textStyle,
-                          atomicStyles.mb8px,
-                        ]}>
-                        {translate('accountInfo.province')}
-                      </Text>
-
-                      <Pressable onPress={handleOpenProvinceChoice}>
-                        <Text
-                          style={[
-                            atomicStyles.h5,
-                            atomicStyles.bold,
-                            styles.textStyle,
-                            atomicStyles.textGray,
-                          ]}>
-                          {province}
-                        </Text>
-                      </Pressable>
-                    </View>
-                  </>
-                )}
-              </View>
-              <View style={[styles.viewContainer, styles.box]}>
-                <LineBlock
-                  label={translate('accountInfo.changePassword.changePassword')}
-                  onPress={handleGoToChangePasswordProfileScreen}
-                  icon={require('assets/icons/LockIcon.svg')}
+                      {moment(dateOfBirth).format('DD/MM/YYYY')}
+                    </Text>
+                  </Pressable>
+                </View>
+                <SvgIcon
+                  component={require('assets/icons/LineHorizontal.svg')}
                 />
-              </View>
+                <View style={[atomicStyles.w100]}>
+                  <Text
+                    style={[
+                      atomicStyles.w50,
+                      atomicStyles.textRight,
+                      atomicStyles.h5,
+                      atomicStyles.bold,
+                      styles.textStyle,
+                      atomicStyles.mb4px,
+                    ]}>
+                    {translate('accountInfo.gender')}
+                  </Text>
 
-              <ButtonMain
-                onPress={updateProfile}
-                label={translate('accountInfo.saveInfo')}
-              />
-            </SafeAreaView>
-          </DefaultLayout>
-        </Animated.View>
-      </Pressable>
+                  <Pressable onPress={handleOpenGenderPick}>
+                    <Text
+                      style={[
+                        atomicStyles.h5,
+                        atomicStyles.bold,
+                        atomicStyles.textRight,
+                        styles.textStyle,
+                        atomicStyles.textGray,
+                        atomicStyles.mb16px,
+                        atomicStyles.mt8px,
+                        atomicStyles.w50,
+                      ]}>
+                      {gender}
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            )}
+
+            {loading ? (
+              <AccountInfoSkeleton />
+            ) : (
+              <>
+                <Dash
+                  dashGap={0}
+                  dashLength={3}
+                  dashThickness={1}
+                  style={[styles.dash, atomicStyles.mb16px]}
+                  dashColor={Colors.Gray}
+                />
+
+                <View
+                  style={[
+                    atomicStyles.flexRow,
+                    atomicStyles.justifyContentBetween,
+                    atomicStyles.mt8px,
+                  ]}>
+                  <Text
+                    style={[
+                      atomicStyles.h5,
+                      atomicStyles.bold,
+                      styles.textStyle,
+                      atomicStyles.mb8px,
+                    ]}>
+                    {translate('accountInfo.province')}
+                  </Text>
+
+                  <Pressable onPress={handleOpenProvincePick}>
+                    <Text
+                      style={[
+                        atomicStyles.h5,
+                        atomicStyles.bold,
+                        styles.textStyle,
+                        atomicStyles.textGray,
+                      ]}>
+                      {province}
+                    </Text>
+                  </Pressable>
+                </View>
+              </>
+            )}
+          </View>
+          <View style={[styles.viewContainer, styles.box]}>
+            <LineBlock
+              label={translate('accountInfo.changePassword.changePassword')}
+              onPress={handleGoToChangePasswordProfileScreen}
+              icon={require('assets/icons/LockIcon.svg')}
+            />
+          </View>
+
+          <ButtonMain
+            onPress={updateProfile}
+            label={translate('accountInfo.saveInfo')}
+            loading={saveLoading}
+          />
+        </SafeAreaView>
+      </DefaultLayout>
     </>
   );
 };
